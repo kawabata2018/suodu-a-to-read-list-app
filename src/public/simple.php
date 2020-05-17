8a747d2258e5e017479d2b7715e55afda3420128
Kawabata <kawabata@MacKB.local>
2020-05-17 22:38:14 +0900

    Update src

--- src/public/simple.php
+++ src/public/simple.php
@@ -1,8 +1,8 @@
 <?php include('templates/header.php'); ?>
 
-<h2 class="center">SUODU is coming soon!</h2>
+<h2 class="text-center">SUODU is coming soon!</h2>
 
-<div class="icon center">
+<div class="icon text-center">
     <img src="public/img/yomimushi.png" alt="よみむし" />
 </div>
 
--- src/public/templates/header.php
+++ src/public/templates/header.php
@@ -2,26 +2,18 @@
     <title>SUODU.WORK</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <style type="text/css">
-    .brand{
-        background: #cbb09c !important;
-        }
-    .brand-text{
-        color: #cbb09c !important;
-    }
-    .icon img{
-        width: 300px;
-        height: auto;
-        margin: 20px auto;
-        padding: 10px;
-    }
-    </style>
+    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
-<body class="grey lighten-4">
-	<nav class="white z-depth-0">
-    <div class="container">
-        <a class="brand-logo brand-text">所読</a>
-    </div>
+<body class="bg-light">
+	<nav class="navbar navbar-expand-lg navbar-light flex-column flex-md-row bg-white z-depth-0">
+        <a class="navbar-brand">
+            <img src="public/img/yomimushi.png" width="50" hspace="10 "class="d-inline-block active" href="/" alt="よみむし"/>
+            <span class="align-middle brand-logo brand-text h3" href="/">所読</span>
+        </a>
+        <ul class="collapse navbar-nav flex-row ml-md-auto d-md-flex">
+            <li class="nav-item">
+                <a class="nav-link text-muted h5">君之所読者何書也？</a>
+            </li>
+        </ul>
     </nav>

0af633d2613ce7d549b812b915b1cfda826d9427
Kawabata <kawabata@MacKB.local>
2020-05-26 21:37:17 +0900

    Add border-navbar

--- src/public/templates/header-lib.php
+++ src/public/templates/header-lib.php
@@ -8,15 +8,3 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body class="bg-book">
-	<nav class="navbar navbar-fixed-top navbar-expand-lg navbar-light flex-md-row bg-white z-depth-0">
-        <a class="navbar-brand" href="/">
-            <img src="/public/img/yomimushi.png" width="50" hspace="10 "class="d-inline-block active" alt="よみむし"/>
-            <span class="align-middle text-brand h3" >所読</span>
-        </a>
-        <ul class="collapse navbar-nav flex-row ml-md-auto d-md-flex">
-            <!-- <li class="nav-item">
-                <span class="align-middle text-muted h5">君之所読者何書也？</span>
-            </li> -->
-        </ul>
-        <!-- <button onclick="location.href='/public/login.php'" class="btn btn-sm btn-outline-dark navbar-btn navbar-right align-middle">ログイン</button> -->
-    </nav>

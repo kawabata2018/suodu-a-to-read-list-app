5a1eef7dbf51f99bb3b6da80ce10d817f503ebd9
Kawabata <kawabata@MacKB.local>
2020-05-27 23:30:24 +0900

    Add 404 & 500 error page

---
+++ src/500.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<head>
+    <title>500</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/public/css/style.css">
+</head>
+<body>
+
+<center class="icon">
+    <h2>500 INTERNAL SERVER ERROR</h2>
+    <img src="/public/img/500.jpg">
+    <div>
+        <a href="/">トップページへ</a>
+    </div>
+</center>
+
+</body>
+</html>

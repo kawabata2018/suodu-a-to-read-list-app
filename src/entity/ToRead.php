9648278738668749dc6255c74cb0400e7c3144e6
Kawabata <kawabata@MacKB.local>
2020-06-01 23:10:50 +0900

    Add ToRead entity

---
+++ src/entity/ToRead.php
@@ -0,0 +1,113 @@
+<?php
+
+class ToRead { 
+    private $toreadId;
+    private $userId;
+    private $isCompleted;       // boolean
+    private $bookName;
+    private $authorName;
+    private $memo;
+    private $colorTag;          // int
+    private $totalPage;         // int
+    private $currentPage;       // int
+    private $completedOn;       // date
+    private $targetDate;        // date
+    private $createdAt;         // timestamp
+    private $updatedAt;         // timestamp
+    private $deleteFlag;        // boolean
+
+    // constructor
+    public function __construct(){
+    }
+
+    // setters
+    public function setToreadId($toreadId){
+        $this->toreadId = $toreadId;
+    }
+    public function setUserId($userId){
+        $this->userId = $userId;
+    }
+    public function setIsCompleted($isCompleted){
+        $this->isCompleted = $isCompleted;
+    }
+    public function setBookName($bookName){
+        $this->bookName = $bookName;
+    }
+    public function setAuthorName($authorName){
+        $this->authorName = $authorName;
+    }
+    public function setMemo($memo){
+        $this->memo = $memo;
+    }
+    public function setColorTag($colorTag){
+        $this->colorTag = $colorTag;
+    }
+    public function setTotalPage($totalPage){
+        $this->totalPage = $totalPage;
+    }
+    public function setCurrentPage($currentPage){
+        $this->currentPage = $currentPage;
+    }
+    public function setCompletedOn($completedOn){
+        $this->completedOn = $completedOn;
+    }
+    public function setTargetDate($targetDate){
+        $this->targetDate = $targetDate;
+    }
+    public function setCreatedAt($createdAt){
+        $this->createdAt = $createdAt;
+    }
+    public function setUpdatedAt($updatedAt){
+        $this->updatedAt = $updatedAt;
+    }
+    public function setDeleteFlag($deleteFlag){
+        $this->deleteFlag = $deleteFlag;
+    }
+
+    // getters
+    public function getToreadId(){
+        return $this->toreadId;
+    }
+    public function getUserId(){
+        return $this->userId;
+    }
+    public function getIsCompleted(){
+        return $this->isCompleted;
+    }
+    public function getBookName(){
+        return $this->bookName;
+    }
+    public function getAuthorName(){
+        return $this->authorName;
+    }
+    public function getMemo(){
+        return $this->memo;
+    }
+    public function getColorTag(){
+        return $this->colorTag;
+    }
+    public function getTotalPage(){
+        return $this->totalPage;
+    }
+    public function getCurrentPage(){
+        return $this->currentPage;
+    }
+    public function getCompletedOn(){
+        return $this->completedOn;
+    }
+    public function getTargetDate(){
+        return $this->targetDate;
+    }
+    public function getCreatedAt(){
+        return $this->createdAt;
+    }
+    public function getUpdatedAt(){
+        return $this->updatedAt;
+    }
+    public function getDeleteFlag(){
+        return $this->deleteFlag;
+    }
+
+}
+
+?>

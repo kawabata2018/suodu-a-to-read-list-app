65c6d418a03b7cde9598c31707aa939606534c4e
Kawabata <kawabata@MacKB.local>
2020-06-13 14:06:47 +0900

    Add session-timeout page

---
+++ src/408.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<head>
+    <title>Session Timeout</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/public/css/style.css">
+</head>
+<body>
+
+<center class="icon">
+    <h1 style="color: tomato">Session Timeout</h1>
+    <h3>セッションが切れました</h3>
+    <img src="/public/img/404.jpg">
+    <div>
+        <a href="/public/login">ログイン画面へ</a>
+    </div>
+</center>
+
+</body>
+</html>
